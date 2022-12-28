b356590a8e95ab6804a719d9fc5449ed6d52d70a
Abhiram R <abhi.darkness@gmail.com>
2022-12-28 23:33:54 +0530

    Santa emoji

--- 2022/whatsapp_reveals.py
+++ 2022/whatsapp_reveals.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 df = pd.read_csv('wa_santas_santees.csv')
 
-st.title('Santa and Santee List')
+st.title('🎅 Broke Bibliophiles Bangalore Secret Santa 2022 - Homenum Revelio !')
 st.table(df)
