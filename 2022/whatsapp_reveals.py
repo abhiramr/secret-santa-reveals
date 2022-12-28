ae756544eb2740c2ab8080801a85bb9ad16f4057
Abhiram R <abhi.darkness@gmail.com>
2022-12-28 23:43:35 +0530

    Update whatsapp_reveals.py

--- 2022/whatsapp_reveals.py
+++ 2022/whatsapp_reveals.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyECRVbMehaNx3Nqs7tFRQGSa1vR5UGe3uYLlNe_uzrd_Vs2HBMvhezx7sI2dftDGNhA-X_jwIYKSU/pub?gid=0&single=true&output=csv')
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTyECRVbMehaNx3Nqs7tFRQGSa1vR5UGe3uYLlNe_uzrd_Vs2HBMvhezx7sI2dftDGNhA-X_jwIYKSU/pub?gid=0&single=true&output=csv',index_col=False)
 
 st.title('🎅 Broke Bibliophiles Bangalore Secret Santa 2022 - Homenum Revelio !')
 st.table(df)
