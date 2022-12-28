a1b3133a3ae71922b9526a80634eacbbb8cf95ad
Abhiram R <abhi.darkness@gmail.com>
2022-12-28 23:39:17 +0530

    Sleep clown

--- 2022/whatsapp_reveals.py
+++ 2022/whatsapp_reveals.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('wa_santas_santees.csv')
+df = pd.read_csv('https://github.com/abhiramr/secret-santa-reveals/blob/main/2022/wa_santas_santees.csv')
 
 st.title('🎅 Broke Bibliophiles Bangalore Secret Santa 2022 - Homenum Revelio !')
 st.table(df)
